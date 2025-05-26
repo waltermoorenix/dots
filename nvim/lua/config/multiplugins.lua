efa1036f30e54fbda558e4c3a3da4571af32cadb
waltermoorenix <vlolxd@gmail.com>
2025-05-27 00:14:46 +0100

    Add files via upload

---
+++ nvim/lua/config/multiplugins.lua
@@ -0,0 +1,118 @@
+-- Description: Plugins with minimal configuration
+return {
+	-- copilot
+	{ "github/copilot.vim" },
+
+	-- Vim-move plugin
+	{ "matze/vim-move" },
+
+	-- tmux movement
+	{ "christoomey/vim-tmux-navigator" },
+
+	-- UI enhancement plugin
+	{
+		"stevearc/dressing.nvim",
+		event = "VeryLazy",
+	},
+
+	-- Cursor smearing
+	{
+		"sphamba/smear-cursor.nvim",
+		opts = {},
+	},
+
+	-- 🧘 Zen mode
+	{
+		"folke/zen-mode.nvim",
+	},
+
+	-- screenkey
+	{
+		"NStefan002/screenkey.nvim",
+		lazy = false,
+		version = "*", -- or branch = "main", to use the latest commit
+	},
+
+	-- Colorizer
+	{
+		"NvChad/nvim-colorizer.lua",
+		opts = {
+			user_default_options = {
+				tailwind = true,
+			},
+		},
+	},
+
+	-- which key
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 500
+		end,
+	},
+
+	-- Tab bar
+	{
+		"akinsho/bufferline.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		version = "*",
+		opts = {
+			options = {
+				mode = "tabs",
+			},
+		},
+	},
+
+	-- Icons picker
+	{
+		"ziontee113/icon-picker.nvim",
+		config = function()
+			require("icon-picker").setup({ disable_legacy_commands = true })
+
+			local opts = { noremap = true, silent = true }
+
+			vim.keymap.set("n", "<Leader><Leader>i", "<cmd>IconPickerNormal<cr>", opts)
+			vim.keymap.set("n", "<Leader><Leader>y", "<cmd>IconPickerYank<cr>", opts) --> Yank the selected icon into register
+		end,
+	},
+
+	-- Noice plugin
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			routes = {
+				{
+					filter = { event = "notify", find = "No information available" },
+					opts = { skip = true },
+				},
+			},
+			presets = {
+				lsp_doc_border = true,
+			},
+		},
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		},
+	},
+
+	-- nvim silicon
+	{
+		"michaelrommel/nvim-silicon",
+		lazy = true,
+		cmd = "Silicon",
+		config = function()
+			require("silicon").setup({
+				font = "JetBrains Mono Nerd Font=34;Noto Color Emoji=34",
+				theme = "Dracula",
+				background = "#94e2d5",
+				window_title = function()
+					return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
+				end,
+			})
+		end,
+	},
+}
---
+++ nvim/lua/config/plugins/lsp.lua
@@ -0,0 +1,149 @@
+return {
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		{ "antosha417/nvim-lsp-file-operations", config = true },
+		{ "folke/neodev.nvim", opts = {} },
+	},
+	config = function()
+		-- Import required plugins
+		local lspconfig = require("lspconfig")
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+		local util = require("lspconfig/util")
+		local on_attach = require("neodev").on_attach
+
+		-- Enhanced capabilities for autocompletion
+		local capabilities = cmp_nvim_lsp.default_capabilities()
+
+		-- Diagnostic signs configuration
+		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			-- vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+			vim.diagnostic.config()
+		end
+
+		-- Format on save
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			callback = function(event)
+				local client = vim.lsp.get_active_clients({ bufnr = event.buf })[1]
+				if client and client.supports_method("textDocument/formatting") then
+					vim.lsp.buf.format({ bufnr = event.buf, async = false })
+				end
+			end,
+		})
+
+		-- LSP key mappings
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+			callback = function(event)
+				local opts = { buffer = event.buf, silent = true }
+				local keymap = vim.keymap
+
+				-- Keybindings for LSP
+				keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+				keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+				keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+				keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+				keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+				keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+				keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+				keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+				keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+				keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+			end,
+		})
+
+		-- LSP server configurations
+		local server_configurations = {
+			lua_ls = {
+				settings = {
+					Lua = {
+						diagnostics = { globals = { "vim" } },
+						completion = { callSnippet = "Replace" },
+					},
+				},
+			},
+			emmet_ls = {
+				filetypes = {
+					"html",
+					"css",
+					"javascript",
+					"javascriptreact",
+					"typescript",
+					"typescriptreact",
+					"svelte",
+				},
+			},
+			clangd = {
+				init_options = { clangdFileStatus = true },
+				filetypes = { "c", "cpp", "objc", "objcpp" },
+			},
+			pyright = {
+				on_attach = on_attach,
+				capabilities = capabilities,
+				filetypes = { "python" },
+			},
+			-- pylint = {
+			-- 	on_attach = on_attach,
+			-- 	capabilities = capabilities,
+			-- 	filetypes = { "python" },
+			-- },
+			ts_ls = {
+				filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+			},
+			tailwindcss = {
+				on_attach = on_attach,
+				capabilities = capabilities,
+			},
+			svelte = {
+				on_attach = function(client, bufnr)
+					vim.api.nvim_create_autocmd("BufWritePost", {
+						pattern = { "*.js", "*.ts" },
+						callback = function(ctx)
+							client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+						end,
+					})
+				end,
+			},
+			graphql = {
+				filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+			},
+			gopls = {
+				on_attach = on_attach,
+				capabilities = capabilities,
+				cmd = { "gopls", "serve" },
+				filetypes = { "go", "gomod", "gowork", "gotmpl" },
+				root_dir = util.root_pattern("go.mod", "go.work", ".git"),
+			},
+			zls = {
+				on_attach = on_attach,
+				capabilities = capabilities,
+				cmd = { "zls" },
+				filetypes = { "zig" },
+			},
+			rust_analyzer = {
+				on_attach = on_attach,
+				capabilities = capabilities,
+				filetypes = { "rust" },
+				root_dir = util.root_pattern("Cargo.toml"),
+				settings = {
+					["rust-analyzer"] = {
+						cargo = {
+							allFeatures = true,
+						},
+					},
+				},
+			},
+		}
+
+		-- Apply server configurations
+		for server, config in pairs(server_configurations) do
+			lspconfig[server].setup(vim.tbl_extend("force", { capabilities = capabilities }, config))
+		end
+	end,
+}
